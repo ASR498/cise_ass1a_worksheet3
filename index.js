6ea66832e76b73c0fdae1e4df983ca40f6c608f6
ASR498 <adityarana498@gmail.com>
2022-08-17 20:10:20 +1200

    heroku deployment index and env

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+//import your models
+require("dotenv").config()
+
+mongoose
+ .connect(
+     process.env.MONGODB_CONNECTION_STRING,
+         {
+           useNewUrlParser: true,
+           useUnifiedTopology: true,
+         }
+ )
+ .then(() => console.log("MongoDB has been connected"))
+ .catch((err) => console.log(err));
+
+ 
+
+//middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//import routes
+require("./routes/quoteRoute.js")(app);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
+
+
